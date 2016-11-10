683700147930d46f9eef3b33bca716783bc99921
Braybaut <brayan.bautista@seaq.co>
2016-11-10 16:30:16 -0500

    add file for check login

---
+++ checklogin.php
@@ -0,0 +1,37 @@
+<?php
+
+$usuario = $_POST['username'];
+$pass = $_POST['password'];
+
+if(empty($usuario) || empty($pass)){
+header("Location: index.html");
+exit();
+}
+
+mysql_connect('localhost','root','Locogreenday666') or die("Error al conectar " . mysql_error());
+mysql_select_db('restaurant') or die ("Error al seleccionar la Base de datos: " . mysql_error());
+
+$result = mysql_query("SELECT * from app_user where user_name='" . $usuario . "'");
+
+if($row = mysql_fetch_array($result)){
+if($row['password'] == $pass){
+session_start();
+$_SESSION['loggedin'] = true;
+$_SESSION['usuario'] = $usuario;
+$_SESSION['start'] = time();
+$_SESSION['expire'] = $_SESSION['start'] + (5 * 60);
+
+
+
+header("Location: options.php");
+}else{
+header("Location: index.html");
+exit();
+}
+}else{
+header("Location: index.html");
+exit();
+}
+
+
+?>
